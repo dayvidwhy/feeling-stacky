eb9a0f4e441620b7db7cf0f69188d44f171a072d
David Young <dayvidwhy@gmail.com>
2016-06-16 21:41:49 +1000

    fix callbacks, update style

--- js/main.js
+++ js/main.js
@@ -1,19 +1,33 @@
 $(document).ready(function() {
 	$("#search").click(function() {
+		//allow people to search again
+		$(".response-field").empty();
+		var search = $('input[name=searching]').val();
+
+		//stop asking dumb questions
+		if (search === '' || search === null) {
+			$("#response").text("You should ask me something.");
+			return;
+		}
+		//let them know we're off finding a solution
 		$("#response").text("Finding your answer");
-		var search = $('input[name=search]').val();
+		search = search.split(' ').join('%20');
+		console.log(search);
 		var query = "https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=activity&q="
 			+search+"&site=stackoverflow";
 		//query for a question id that goes with the query
 		$.getJSON(query, function(data){
 			console.log(data);
 			var id = data.items[0].question_id;
-			console.log(id);
+			$("#title-response").text(data.items[0].title);
+			$("#title-url").html('<a class="answer" href="'+data.items[0].link+'">'+data.items[0].link+'</a>');
+			console.log("found question id "+id);
 			var questToAnswer = "https://api.stackexchange.com/2.2/questions/"+id+"/answers?order=desc&sort=activity&site=stackoverflow"
 			//use that question id to find an answer id
 			$.getJSON(questToAnswer, function(data) {
 				console.log(data);
 				var id = data.items[0].answer_id;
+				console.log("found answer id "+id);
 				var answerBody ="https://api.stackexchange.com/2.2/answers/"+id+"?order=desc&sort=activity&site=stackoverflow&filter=withbody";
 				//take that answer id and find a chunk of text associated with it
 				$.getJSON(answerBody, function(data) {
@@ -23,4 +37,4 @@ $(document).ready(function() {
 			});
 		});
 	});
-});
+}); //hadouken
