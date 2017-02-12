9f3f2115b2f09bde00d26112448819d8a75abe60
dayvidwhy <dayvidwhy@gmail.com>
2017-02-12 18:39:56 +1000

    Largely clean up JS code.

--- js/main.js
+++ js/main.js
@@ -1,48 +1,69 @@
-$(document).ready(function() {
-	
+/*
+* Start the searches
+*/
+function init() {
 	//this binds a more lenient query to the serious button
-	$("#lucky").click({type: "q"},query);
+	$("#lucky").click({type: "q"}, query);
 	//this binds a more literal query to lucky button
-	$("#serious").click({type: "title"},query);
+	$("#serious").click({type: "title"}, query);
+}
 
-	function query(event) {
-		//console.log(event.data.type);
-		//allow people to search again
-		$(".response-field").empty();
-		var search = $('input[name=searching]').val();
-		//stop asking dumb questions
-		if (search === '' || search === null) {
-			$("#title-response").text("You should ask me something.");
-			return;
-		}
-		
-		//let them know we're off finding a solution
-		$("#title-response").text("Finding your answer");
-		search = search.split(' ').join('%20');
-		//console.log(search);
-		var query = "https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=activity&"+event.data.type+"="
-		+search+"&accepted=True&site=stackoverflow";
-		
-		//query for an accepted answer id that goes with the query
-		$.getJSON(query, function(data){
-			//console.log(data);
-			if (data.items.length > 0) {
-				//we need the id of the top answer, this is the first element of the items array.
-				var id = data.items[0].accepted_answer_id;
-				//console.log("found answer id "+id);
-				$("#title-response").text(data.items[0].title);
-				$("#title-url").html("<a href=\""+data.items[0].link+"\">"+data.items[0].link+"</a>");
-				var answerBody ="https://api.stackexchange.com/2.2/answers/"+id+"?order=desc&sort=activity&site=stackoverflow&filter=withbody";
-				//take that answer id and find a chunk of text associated with it
-				$.getJSON(answerBody, function(data) {
-					//console.log(data);
-					$("#response").html(data.items[0].body);
-					return;
-				});
-			} else {
-				$("#response").text("There was no answer to this question :(");
-					return;
-			}
-		});
+/*
+* Initial query
+*/
+function query(event) {
+	//allow people to search again
+	$(".response-field").empty();
+
+	var search = $('input[name=searching]').val();
+	
+	if (!search) {
+		// no entry
+		$("#title-response").text("You should ask me something.");
+		return;
 	}
-});
+	
+	//let them know we're off finding a solution
+	$("#title-response").text("Finding your answer");
+	var query = "https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=activity&"
+		+ event.data.type
+		+ "="
+		+ search
+		+ "&accepted=True&site=stackoverflow";
+	
+	//query for an accepted answer id that goes with the query
+	$.getJSON(encodeURI(query), firstResponse);
+}
+
+/*
+* Get some things back.
+*/
+function firstResponse(data) {
+	if (data.items.length <= 0) {
+		$("#response").text("There was no answer to this question :(");
+		return;
+	} 
+	
+	//we need the id of the top answer, this is the first element of the items array.
+	var id = data.items[0].accepted_answer_id;
+
+
+	$("#title-response").text(data.items[0].title);
+	$("#title-url").html('<a href="' + data.items[0].link + '">' + data.items[0].link + '</a>');
+	var answerBody = "https://api.stackexchange.com/2.2/answers/"
+		+ id
+		+ "?order=desc&sort=activity&site=stackoverflow&filter=withbody";
+
+	//take that answer id and find a chunk of text associated with it
+	$.getJSON(answerBody, secondResponse);
+}
+
+/*
+* Second query returns actual answer.
+*/
+function secondResponse(data) {
+	$("#response").html(data.items[0].body);
+	return;
+}
+
+$(document).ready(init);
