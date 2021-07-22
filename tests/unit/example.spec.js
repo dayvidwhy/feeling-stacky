a9f0617f09b5814c71c5f89fdd5cc52ea4e6b55f
dayvidwhy <dayvidwhy@gmail.com>
2021-07-22 21:23:06 +1000

    Use simple webpack config over vue-cli.

--- tests/unit/example.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils'
-import banner from '@/components/banner.vue'
-
-describe('banner.vue', () => {
-  it('renders correctly', () => {
-    const wrapper = shallowMount(banner);
-    expect(wrapper).toMatchSnapshot();
-  });
-});
