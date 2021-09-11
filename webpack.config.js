75aeb2171cb501d1e4c11f65755bf9a5e9d1eadc
dayvidwhy <dayvidwhy@gmail.com>
2021-09-11 21:11:07 +1000

    Dependency updates.

--- webpack.config.js
+++ webpack.config.js
@@ -5,94 +5,97 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const autoprefixer = require("autoprefixer");
 const path = require("path");
 
-module.exports = {
-    entry: {
-        main: "./src/main.js",
-    },
-    output: {
-        filename: "[name].[contenthash:8].js",
-        path: path.resolve(__dirname, "dist"),
-        chunkFilename: "[name].[contenthash:8].js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                },
-            },
-            {
-                test: /\.vue$/,
-                loader: "vue-loader",
-            },
-            {
-                test: /\.(eot|ttf|woff|woff2)(\?\S*)?$/,
-                loader: "file-loader",
-                options: {
-                    name: "[name][contenthash:8].[ext]",
-                },
-            },
-            {
-                test: /\.(png|jpe?g|gif|webm|mp4|svg)$/,
-                loader: "file-loader",
-                options: {
-                    name: "[name][contenthash:8].[ext]",
-                    outputPath: "assets/img",
-                    esModule: false,
-                },
-            },
-            {
-                test: /\.s?css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            plugins: () => [autoprefixer()],
-                        },
+module.exports = (env, argv) => {
+    const prod = argv.mode === "production";
+    return {
+        entry: {
+            main: "./src/main.js",
+        },
+        output: {
+            filename: prod ? '[name].[contenthash:8].js' : '[name].[hash:8].js',
+            path: path.resolve(__dirname, "dist"),
+            chunkFilename: "[name].[contenthash:8].js"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
                     },
-                    "sass-loader"
-                ],
-            },
-        ],
-    },
-    plugins: [
-        new VueLoaderPlugin(),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: "[name].[contenthash:8].css",
-            chunkFilename: "[name].[contenthash:8].css"
-        }),
-        new htmlWebpackPlugin({
-            template: path.resolve(__dirname, "public", "index.html"),
-            favicon: "./public/favicon.ico"
-        }),
-    ],
-    resolve: {
-        alias: {
-            vue$: "vue/dist/vue.runtime.esm.js",
-            "@": path.resolve(__dirname, 'src')
+                },
+                {
+                    test: /\.vue$/,
+                    loader: "vue-loader",
+                },
+                {
+                    test: /\.(eot|ttf|woff|woff2)(\?\S*)?$/,
+                    loader: "file-loader",
+                    options: {
+                        name: "[name][contenthash:8].[ext]",
+                    },
+                },
+                {
+                    test: /\.(png|jpe?g|gif|webm|mp4|svg)$/,
+                    loader: "file-loader",
+                    options: {
+                        name: "[name][contenthash:8].[ext]",
+                        outputPath: "assets/img",
+                        esModule: false,
+                    },
+                },
+                {
+                    test: /\.s?css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        "css-loader",
+                        {
+                            loader: "postcss-loader",
+                            options: {
+                                plugins: () => [autoprefixer()],
+                            },
+                        },
+                        "sass-loader"
+                    ],
+                },
+            ],
         },
-        extensions: ["*", ".js", ".vue", ".json"],
-    },
-    optimization: {
-        moduleIds: "hashed",
-        runtimeChunk: "single",
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: "vendors",
-                    priority: -10,
-                    chunks: "all",
+        plugins: [
+            new VueLoaderPlugin(),
+            new CleanWebpackPlugin(),
+            new MiniCssExtractPlugin({
+                filename: "[name].[contenthash:8].css",
+                chunkFilename: "[name].[contenthash:8].css"
+            }),
+            new htmlWebpackPlugin({
+                template: path.resolve(__dirname, "public", "index.html"),
+                favicon: "./public/favicon.ico"
+            }),
+        ],
+        resolve: {
+            alias: {
+                vue$: "vue/dist/vue.runtime.esm.js",
+                "@": path.resolve(__dirname, 'src')
+            },
+            extensions: ["*", ".js", ".vue", ".json"],
+        },
+        optimization: {
+            moduleIds: "hashed",
+            runtimeChunk: "single",
+            splitChunks: {
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: "vendors",
+                        priority: -10,
+                        chunks: "all",
+                    },
                 },
             },
         },
-    },
-    devServer: {
-        historyApiFallback: true,
-    },
+        devServer: {
+            historyApiFallback: true,
+        },
+    }
 };
